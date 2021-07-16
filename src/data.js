631b43e4b6555e640f03a3e4764dcf42fc5b624e
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-16 16:48:15 +0530

    Updated data.js

--- src/data.js
+++ src/data.js
@@ -1,52 +1,45 @@
 import React from 'react'
-import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
-export const links = [
-  {
-    id: 1,
-    url: '/',
-    text: 'random',
-  },
-  {
-    id: 2,
-    url: '/about',
-    text: 'about',
-  },
-  {
-    id: 3,
-    url: '/projects',
-    text: 'projects',
-  },
-  {
-    id: 4,
-    url: '/contact',
-    text: 'contact',
-  },
-  {
-    id: 5,
-    url: '/profile',
-    text: 'profile',
-  },
+import { FaBehance, FaFacebook, FaLinkedin, FaGithub } from 'react-icons/fa'
+export const links = [{
+        id: 1,
+        url: '/',
+        text: 'random',
+    },
+    {
+        id: 2,
+        url: '/about',
+        text: 'about',
+    },
+    {
+        id: 3,
+        url: '/projects',
+        text: 'projects',
+    },
+    {
+        id: 4,
+        url: '/contact',
+        text: 'contact',
+    },
+    {
+        id: 5,
+        url: '/profile',
+        text: 'profile',
+    },
 ]
 
-export const social = [
-  {
-    id: 1,
-    url: 'https://www.twitter.com',
-    icon: <FaFacebook />,
-  },
-  {
-    id: 2,
-    url: 'https://www.twitter.com',
-    icon: <FaTwitter />,
-  },
-  {
-    id: 3,
-    url: 'https://www.twitter.com',
-    icon: <FaLinkedin />,
-  },
-  {
-    id: 4,
-    url: 'https://www.twitter.com',
-    icon: <FaBehance />,
-  },
+export const social = [{
+        id: 1,
+        url: 'https://www.facebook.com/profile.php?id=100057356125529',
+        icon: < FaFacebook / > ,
+    },
+    {
+        id: 2,
+        url: 'https://github.com/arun-kushwaha04',
+        icon: < FaGithub / > ,
+    },
+    {
+        id: 3,
+        url: 'https://www.linkedin.com/in/arun-singh-kushwaha-087652207/',
+        icon: < FaLinkedin / > ,
+    },
 ]
