62e6d2aee8565d0ca211e951e3d40cb6e1fceee2
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-16 16:53:20 +0530

    Updates

---
+++ precache-manifest.96a15bec1dcbf55868fd5b6235e238bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37af0551f891fe6e4518c80953c2aad",
+    "url": "/Navbar-React-Project/index.html"
+  },
+  {
+    "revision": "f856064e8c861a307e5e",
+    "url": "/Navbar-React-Project/static/css/main.d1743f3b.chunk.css"
+  },
+  {
+    "revision": "44fad4bd07e64b40f269",
+    "url": "/Navbar-React-Project/static/js/2.81c645b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Navbar-React-Project/static/js/2.81c645b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f856064e8c861a307e5e",
+    "url": "/Navbar-React-Project/static/js/main.1e974ec8.chunk.js"
+  },
+  {
+    "revision": "7a1940b1d1ece87f2020",
+    "url": "/Navbar-React-Project/static/js/runtime-main.80ad1807.js"
+  },
+  {
+    "revision": "0eb8b5ca7c0a2529dd3ec74e77726b1e",
+    "url": "/Navbar-React-Project/static/media/logo.0eb8b5ca.svg"
+  }
+]);
